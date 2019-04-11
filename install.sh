fce5d7e8145b613800952f84f5a382f97d3d098e
mbobysetiawan <bobysetiawan156@gmail.com>
2019-04-11 20:33:54 +0700

    seeker

---
+++ install.sh
@@ -0,0 +1,22 @@
+echo '[!] Updating...'
+apt-get update > install.log
+echo
+echo '[!] Installing Dependencies...'
+echo '    Python'
+apt-get -y install python &>> install.log
+echo '    PHP'
+apt-get -y install php &>> install.log
+echo '    wget'
+apt-get -y install wget &>> install.log
+echo '    unzip'
+apt-get -y install unzip &>> install.log
+echo '    openssh'
+apt-get -y install ssh &>> install.log
+echo '    Requests'
+pip install requests &>> install.log
+echo
+echo '[!] Setting Permissions...'
+chmod 777 template/nearyou/php/info.txt
+chmod 777 template/nearyou/php/result.txt
+echo
+echo '[!] Installed.'
